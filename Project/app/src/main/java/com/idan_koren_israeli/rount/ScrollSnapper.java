105fb24114cb0a006a92d306ee6118b664ea5fce
Idan Koren-Israeli <39092077+Idan503@users.noreply.github.com>
2020-08-02 12:50:01 +0300

    Added snapping functionality to custom scrollview on main screen

--- Project/app/src/main/java/com/idan_koren_israeli/rount/ScrollSnapper.java
+++ Project/app/src/main/java/com/idan_koren_israeli/rount/ScrollSnapper.java
@@ -1,19 +1,25 @@
 package com.idan_koren_israeli.rount;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.ScaleDrawable;
+import android.os.CountDownTimer;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 //This is a custom view for Rount selection on main menu
 
@@ -21,12 +27,19 @@ class ScrollSnapper extends ScrollView
 {
 
     final float CARD_MAX_SIZE_MULT = 1.1f;
-    final float CARD_MIN_SIZE_MULT = 0.8f;
+    final float CARD_MIN_SIZE_MULT = 0.85f;
     int[] anchorPos = new int[2]; //Cordintes where the card should be the biggest.
     int[] childPos = new int[2]; //this array will store x,y values of children
     boolean firstRender = true; //Flag for detecting first frame that is being drawn
     LinearLayout parent;
 
+    // Using this timer to detect when user scroll is finished
+    Timer ntimer = new Timer();
+    MotionEvent event;
+    boolean isSnapping = false;
+    int targetSnapY = 0;
+
+    RountCard currentSelected = null;
     ArrayList<RountCard> rountCards = new ArrayList<>();
     // All cards inflated will be saved as objects
 
@@ -59,6 +72,12 @@ class ScrollSnapper extends ScrollView
             rountCards.add(new RountCard(newCard,context));
         }
 
+        //Resetting visability to update parent layout size to wrap all content
+        parent.setVisibility(View.GONE);
+        parent.setVisibility(View.VISIBLE);
+
+        currentSelected = rountCards.get(0);
+
 
 
     }
@@ -74,13 +93,24 @@ class ScrollSnapper extends ScrollView
         }
 
         for(RountCard rountCard : rountCards){
-            if(rountCard.getCard()==parent.getChildAt(0))
-                System.out.println("Wow is " + getChildSelectedPercent(rountCard.getCard()));
+            float newPercentValue = getChildSelectedPercent(rountCard.getCard());
             rountCard.update(getChildSelectedPercent(rountCard.getCard()));
+            if(rountCard!=currentSelected && newPercentValue >  currentSelected.getSelectedPercent()) {
+                //We have a new selected card.
+                currentSelected = rountCard;
+            }
         }
 
-    }
+        for(RountCard card : rountCards){
+            if(card==currentSelected)
+                card.getRountName().setText("Selected!");
+            else
+                card.getRountName().setText("Not Selected!");
+        }
 
+
+
+    }
 
 
     //This function calculates distance between 2 vectors represented as two [2] arrays
@@ -108,4 +138,56 @@ class ScrollSnapper extends ScrollView
 
 
 
+    //Detecting user finishes scroll to perform snap to selected card
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt)
+    {
+        checkAgain();
+        super.onScrollChanged(l, t, oldl, oldt);
+    }
+
+    public void checkAgain(){
+        try{
+            ntimer.cancel();
+            ntimer.purge();
+        }
+        catch(Exception e){e.printStackTrace();}
+        ntimer = new Timer();
+        ntimer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                if(event.getAction() == MotionEvent.ACTION_UP && !isSnapping) {
+                    if (isSelectedViewFarEnough()) {
+                        // ScrollView Stopped Scrolling and Finger is not on the ScrollView
+                        targetSnapY = (int) currentSelected.getCard().getY();
+                    }
+                    scrollTo(0, targetSnapY); //Auto Scrolling to the selected card
+                    isSnapping = true;
+                }
+                else{
+                    // ScrollView Stopped Scrolling But Finger is still on the ScrollView
+                    checkAgain();
+                }
+            }
+        },100);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.event = event;
+        isSnapping = false;
+        return super.onTouchEvent(event);
+    }
+
+
+    //Checking if current selected card is far enough from the target to make a snap
+    //Added this to fix a bug in google pixel phones - scrollview rotated because of 1 coordinate difference
+    public boolean isSelectedViewFarEnough(){
+        return (Math.abs(currentSelected.getCard().getY()-targetSnapY) > 2);
+    }
 }
+
+
+
