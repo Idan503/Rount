98964f2c5280c3581bb19925c2caed8932e1fc89
Idan Koren-Israeli <39092077+Idan503@users.noreply.github.com>
2020-08-06 15:25:08 +0300

    Started UI of create rount using input fragment

--- Project/app/src/main/java/com/idan_koren_israeli/rount/NavigationBar.java
+++ Project/app/src/main/java/com/idan_koren_israeli/rount/NavigationBar.java
@@ -1,18 +1,19 @@
 package com.idan_koren_israeli.rount;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link create_rount_input#newInstance} factory method to
+ * Use the {@link NavigationBar#newInstance} factory method to
  * create an instance of this fragment.
- *
  */
-public class create_rount_input extends Fragment {
+public class NavigationBar extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,26 +24,26 @@ public class create_rount_input extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    public NavigationBar() {
+        // Required empty public constructor
+    }
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment create_rount_input.
+     * @return A new instance of fragment NavigationBar.
      */
     // TODO: Rename and change types and number of parameters
-    public static create_rount_input newInstance(String param1, String param2) {
-        create_rount_input fragment = new create_rount_input();
+    public static NavigationBar newInstance(String param1, String param2) {
+        NavigationBar fragment = new NavigationBar();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    public create_rount_input() {
-        // Required empty public constructor
     }
 
     @Override
@@ -58,6 +59,6 @@ public class create_rount_input extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_rount_input, container, false);
+        return inflater.inflate(R.layout.fragment_navigation_bar, container, false);
     }
 }
